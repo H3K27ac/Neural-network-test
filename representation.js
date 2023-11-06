3292a93e2c166ad35305c4b5f148c79d8b937b16
Test <101502649+susamobus@users.noreply.github.com>
2023-11-06 15:30:36 +0800

    Create representation.js

---
+++ representation.js
@@ -0,0 +1,13 @@
+let weights = {}
+let neurons = {}
+let biases = {}
+let layers;
+
+function CreateGraph() {
+  let input = document.getElementById("structure").value
+  let array = input.replace(/[{}]/g, '').split(',').map(item => Number(item.trim()));
+  layers = array.length
+  document.getElementById("layers").innerHTML = layers
+}
+
+
