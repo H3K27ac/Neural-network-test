963266538bd41a21801bf6b23dd1b7fe7a90260a
Test <101502649+susamobus@users.noreply.github.com>
2023-11-06 16:49:16 +0800

    Update representation.js

--- representation.js
+++ representation.js
@@ -8,19 +8,20 @@ function CreateGraph() {
   let structure = input.replace(/[{}]/g, '').split(',').map(item => parseInt(item));
   layers = structure.length
   for (let i=0; i<layers; i++) {
-    let subarray = [];
+  //  let subarray = [];
     let column = document.createElement("div")
     column.className = "column"
     column.id = "column " + i
+    document.getElementById("layers").innerHTML = "L"
     for (let j=0; j<structure[i]; j++) {
       let neuron = document.createElement("div")
       neuron.className = "neuron"
       neuron.id = "neuron " + i + "," + j
       column.appendChild(neuron)
-      subarray.append(0)
+   //   subarray.append(0)
     }
     document.getElementById("container").appendChild(column)
-    neurons.append(subarray)
+   // neurons.append(subarray)
   }
   document.getElementById("layers").innerHTML = layers
 }
