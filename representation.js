e1cec685a8a1d780b0cb5f760f174724310be29d
Test <101502649+susamobus@users.noreply.github.com>
2023-11-06 16:24:20 +0800

    Update representation.js

--- representation.js
+++ representation.js
@@ -7,9 +7,9 @@ function CreateGraph() {
   let input = document.getElementById("structure").value
   let structure = input.replace(/[{}]/g, '').split(',').map(item => parseInt(item));
   layers = structure.length
-  for (i=0,i<layers,i++) {
+  for (let i=0; i<layers; i++) {
     let subarray = [];
-    for (j=0,j<structure[i],j++) {
+    for (let j=0; j<structure[i]; j++) {
       CreateNeurons(i,j)
       subarray.append(0)
     }
