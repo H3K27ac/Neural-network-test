2c9e4297e63af774d968dd01e8fa5d5798d48232
Test <101502649+susamobus@users.noreply.github.com>
2023-11-07 09:41:45 +0800

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -1,19 +1,19 @@
 
-//function Activation(input) {
-//  return 1 / (1 + (Math.E ** (-1 * input)))
-//}
+function Activation(input) {
+  return 1 / (1 + (Math.E ** (-1 * input)))
+}
 
 function ForwardPass() {
+  let sum;
   for (let i=0; i<layers; i++) {
     for (let j=0; j<structure[i+1]; j++) {
-      document.getElementById("layers").innerHTML = "even more broken"
-      let sum = 0;
+      sum = 0
       for (let k=0; k<structure[i]; k++) {
         sum += weights[i+1][j][k] * neurons[i][k]
       }
       sum += biases[i+1]
       document.getElementById("layers").innerHTML = "broken"
-      neurons[i+1][j] = 1 / (1 + (Math.E ** (-1 * sum)))
+      neurons[i+1][j] = Activation(sum)
     }
   }
   UpdateColor()
