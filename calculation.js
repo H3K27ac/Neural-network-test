97f348cc067f2aff5219896d46be9af0c3ed7251
Test <101502649+susamobus@users.noreply.github.com>
2023-11-07 10:28:50 +0800

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -3,6 +3,10 @@ function Activation(input) {
   return 1 / (1 + (Math.E ** (-1 * input)))
 }
 
+function DerivativeActivation(input) {
+  return Activation(input) * (1 - Activation(input))
+}
+
 function ForwardPass() {
   let sum;
   for (let i=0; i<layers; i++) {
@@ -25,8 +29,19 @@ function NeuronCost(i,j) {
   }
   let sum = 0;
   for (k=0; k<structure[i+1]; k++) {
-    sum += weights[i+1] 
+    sum += weights[i+1] * DerivativeActivation(neurons2[i+1][j]) * NeuronCost(i+1,j)
   }
+  return sum
 }
 
+function WeightCost(i,j,k) {
+  return neurons[i-1][k] * DerivativeActivation(neurons2[i][j]) * NeuronCost(i,j)
+}
+
+function BiasCost(i,j) {
+  return DerivativeActivation(neurons2[i][j]) * NeuronCost(i,j)
+}
+
+function Backprop() {
+}
 
