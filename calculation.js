1e6d7ff942d6759737666076ea44af1ac35ae0c7
Test <101502649+susamobus@users.noreply.github.com>
2023-11-07 10:44:05 +0800

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -16,7 +16,6 @@ function FeedForward() {
         sum += weights[i+1][j][k] * neurons[i][k]
       }
       sum += biases[i+1][j]
-      document.getElementById("layers").innerHTML = "broken"
       neurons[i+1][j] = Activation(sum)
     }
   }
@@ -25,10 +24,11 @@ function FeedForward() {
 
 function NeuronCost(i,j) {
   if (i == layers-1) {
-    return 2* (targets[j] - neurons[i][j])
+    return 2 * (targets[j] - neurons[i][j])
   }
   let sum = 0;
   for (let k=0; k<structure[i+1]; k++) {
+    document.getElementById("layers").innerHTML = "broken but better"
     sum += weights[i+1] * DerivativeActivation(neurons2[i+1][j]) * NeuronCost(i+1,j)
   }
   return sum
@@ -46,11 +46,14 @@ function Backprop() {
   FeedForward()
   for (let i=0; i<layers; i++) {
     for (let j=0; j<structure[i+1]; j++) {
+      document.getElementById("layers").innerHTML = "broken"
       biases[i+1][j] -= learnrate * BiasCost(i+1,j)
       for (let k=0; k<structure[i]; k++) {
+        document.getElementById("layers").innerHTML = "best broken"
         weights[i+1][j][k] -= learnrate * WeightCost(i+1,j,k)
       }
     }
   }
+  UpdateColor()
 }
 
