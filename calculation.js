36ded6ca22efd775ada14d375fdbd9d8855960a4
Test <101502649+susamobus@users.noreply.github.com>
2023-11-08 15:55:53 +0800

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -61,14 +61,14 @@ function BatchForwardPass() {
         sum = 0
         for (let k=0; k<structure[i]; k++) {
           sum += weights[i+1][j][k] * neurons[i][k][n]
+          if (n==0) {
+        document.getElementById("training").innerHTML = sum 
+        }
         }
         sum += biases[i+1][j]
         neurons2[i+1][j][n] = sum
         batch[i+1][j][n] = Activation(sum)
         batchsum += Activation(sum)
-        if (n==0) {
-        document.getElementById("training").innerHTML = sum 
-        }
       }
       batchmean[i+1][j] = batchsum / batchsize
       batchsum = 0
