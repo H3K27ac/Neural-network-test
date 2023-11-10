1f63d292a14ffda2eb7209412ccc825eb0ea5e30
Test <101502649+susamobus@users.noreply.github.com>
2023-11-10 15:47:41 +0800

    Update input.js

--- input.js
+++ input.js
@@ -222,8 +222,8 @@ function MakeDraggable(i) {
           document.getElementById("layers").innerHTML = "snapping"
           isSnapped = true
           document.getElementById("layers").innerHTML = "set"
-          ghost.offsetLeft = x3
-          ghost.offsetTop = height
+          ghost.style.left = x3
+          ghost.style.top = height
         }
       }
       /*
