c95cc56c914b9799042b5716db2b07a102d18441
Test <101502649+susamobus@users.noreply.github.com>
2023-11-10 10:28:42 +0800

    Create input.js

---
+++ input.js
@@ -0,0 +1,30 @@
+let layertypes = ["activationlayer","batchnormlayer"];
+let layernames = ["Activation layer", "Batch normalisation"];
+
+
+function ReplenishLayers() {
+  for (let i=0; i<layertypes.length; i++) {
+    if (document.getElementById(layertypes[i] + "incontainer") == null) {
+      let container = document.getElementById("layercontainer");
+      let layer = document.createElement("div");
+      let layertext = document.createElement("span");
+      layer.className = "layerincontainer"
+      layer.id = layertypes[i] + "incontainer"
+      layertext.innerHTML = layernames[i]
+      layer.appendChild(layertext)
+      container.appendChild(layer);
+    }
+  }
+  UpdateContainerWidth();
+}
+
+function UpdateContainerWidth() {
+  let container = document.getElementById("layercontainer");
+  let children = container.children;
+  let width = 0;
+  for (let i=0; i<children.length; i++) {
+    width += children[i].offsetWidth;
+  }
+  container.style.width = width + 'px'
+}
+
