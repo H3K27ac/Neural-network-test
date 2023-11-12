077383c8b73023a0e72843b540f6f735957b0302
Test <101502649+susamobus@users.noreply.github.com>
2023-11-12 16:39:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let batching = false;
 let infering = false;
+let batchnormindex;
 let neurons3 = [];
 let neurons4 = [];
 
@@ -10,7 +11,11 @@ function ActivationLayer(input,i,j,m) {
 //  }
   switch (layerorder[m+1]) {
     case "batchnormlayer": 
-      return BatchInference(result2,i,j,m+1)
+      if (infering) {
+        return BatchInference(result2,i,j,m+1)
+      } else {
+        return Activation(input,i)
+      }
     default:
       return Activation(input,i)
   }
@@ -28,7 +33,7 @@ function BatchInference(input,i,j,m) {
 }
 
 function GeneralInference() {
-  ClearNeurons()
+  infering = true
   let sum;
   for (let i=0; i<layers-1; i++) {
     let j2 = structure[i+1];
@@ -68,7 +73,7 @@ function GeneralInference() {
 
 
 function GeneralForward() {
-  ClearNeurons()
+  infering = false
   let sum;
   for (let i=0; i<layers-1; i++) {
     let j2 = structure[i+1];
@@ -94,27 +99,72 @@ function GeneralForward() {
 }
 
 function BatchGeneralForward() {
-  ClearNeurons()
+  infering = false
   let sum;
-  for (let i=0; i<layers-1; i++) {
+  let batchsum;
+  let batchsum2;
+  for (let i=0; i<layers; i++) {
     let j2 = structure[i+1];
     for (let j=0; j<j2; j++) {
-      sum = 0
-      let k2 = structure[i];
-      for (let k=0; k<k2; k++) {
-        sum += weights[i+1][j][k] * neurons[i][k]
-      }
-      sum += biases[i+1][j]
-      let result;
-      switch (layerorder[0]) {
+      batchsum = 0;
+      batchsum2 = 0;
+      for (let n=0; n<batchsize; n++) {
+        sum = 0;
+        let k2 = structure[i];
+        for (let k=0; k<k2; k++) {
+          sum += weights[i+1][j][k] * neurons[i][k][n]
+        }
+        sum += biases[i+1][j]
+     //   neurons2[i+1][j][n] = sum
+        let result;
+        switch (layerorder[0]) {
         case "activationlayer":
           result = ActivationLayer(sum,i,j,0)
+          break;
+        case "batchnormlayer":
+          result = sum
           break;
         default:
           result = sum
           break;
+        }
+        if (batchnorm) {
+        batch[i+1][j][n] = result
+        batchsum += result
+        } else {
+          neurons[i+1][j][n] = Math.min(1, Math.max(0, result))
+        }
       }
-      neurons[i+1][j] = Math.min(1, Math.max(0, result))
+      if (batchnorm) {
+      let tempmean = batchsum / batchsize
+      batchmean[i+1][j] = tempmean
+      for (let n=0; n<batchsize; n++) {
+        batchsum2 += (batch[i+1][j][n] - tempmean) ** 2
+      }
+      let tempvar = batchsum2 / batchsize
+      batchvar[i+1][j] = tempvar
+      let tempgamma = batchgamma[i+1][j]
+      let tempbeta = batchbeta[i+1][j]
+      let tempnorm2 = Math.sqrt(tempvar + epsilon)
+      for (let n=0; n<batchsize; n++) {
+        let tempnorm = (batch[i+1][j][n] - tempmean) / tempnorm2
+        batchnormed[i+1][j][n] = tempnorm
+        let result2 = tempgamma * tempnorm + tempbeta
+        let result3;
+        switch (layerorder[batchnormindex+1]) {
+        case "activationlayer":
+          result3 = ActivationLayer(result2,i,j,batchnormindex+1)
+          break;
+        default:
+          result3 = result2
+          break;
+        }
+        neurons[i+1][j][n] = Math.min(1, Math.max(0, result3))
+      }
+      batchmeanmoving[i+1][j] = ((1 - batchalpha) * batchmeanmoving[i+1][j]) + (batchalpha * tempmean) 
+      batchvarmoving[i+1][j] = ((1 - batchalpha) * batchvarmoving[i+1][j]) + (batchalpha * tempvar)
+      }
     }
   }
 }
+
