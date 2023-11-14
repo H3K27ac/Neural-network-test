f1925f942759a3ee9395dae4b0727a5ef049c712
Test <101502649+susamobus@users.noreply.github.com>
2023-11-14 11:49:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -187,7 +187,7 @@ function TestBackprop() {
     testactcache[i+1] = TestDerivativeActivation(testneurons2[i+1],i+1)
     document.getElementById("layers").innerHTML = "biases" + JSON.stringify(testneurons) + JSON.stringify(testweights) + i
     testbiases[i+1] = nj.clip(nj.subtract(testbiases[i+1],nj.multiply(TestBiasCost(i+1),learnrate)),biasrange * -1,biasrange)
-    document.getElementById("layers").innerHTML = "weight" + testweights[i+1] + nj.multiply(TestWeightCost(i+1),learnrate) + nj.zeros([2]).subtract(nj.array([-0.5,-0.5]))
+    document.getElementById("layers").innerHTML = "weight" + JSON.stringify(testweights) + nj.multiply(TestWeightCost(i+1),learnrate) + i
     testweights[i+1] = nj.clip(testweights[i+1].subtract(nj.multiply(TestWeightCost(i+1),learnrate)),weightrange * -1,weightrange)
     //  (l1strength * Math.sign(weights[i+1][j][k])) + (l2strength * (weights[i+1][j][k] ** 2))
   }
