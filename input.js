23d15a4eec30d9f6c6f62259d52cb23c3fc20625
Test <101502649+susamobus@users.noreply.github.com>
2023-11-18 09:42:25 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,10 +1,10 @@
-let layertypes = ["connectedlayer","activationlayer","dropoutlayer","batchnormlayer","layernorm"];
-let layernames = ["Fully connected layer","Activation layer","Dropout layer","Batch normalisation","Layer normalisation"];
-let layercolor = ["white","lightgray","lightgreen","lightblue","blue"];
-let modifytypes = [[],[],["dropoutlayer","dropconnect"],["batchnormlayer","batchrenorm"],["layernorm","instancenorm","groupnorm"]];
-let modifynames = [[],[],["Dropout layer","Dropconnect layer"],["Batch normalisation","Batch renormalisation"],["Layer normalisation","Instance normalisation","Group normalisation"]];
-let modifynames2 = [[],[],["DL","DCL"],["BN","BRN"],["LN","IN","GN"]];
-let modifycolor = [[],[],["lightgreen","yellowgreen"],["lightblue","deepskyblue"],["blue","steelblue","slateblue"]];
+let layertypes = ["connectedlayer","activationlayer","dropoutlayer","batchnormlayer","layernorm","weightnorm"];
+let layernames = ["Fully connected layer","Activation layer","Dropout layer","Batch normalisation","Layer normalisation","Weight normalisation"];
+let layercolor = ["white","lightgray","lightgreen","lightblue","blue","mediumslateblue"];
+let modifytypes = [[],[],["dropoutlayer","dropconnect"],["batchnormlayer","batchrenorm","batchkalman"],["layernorm","instancenorm","groupnorm"],["weightnorm","weightstand"];
+let modifynames = [[],[],["Dropout layer","Dropconnect layer"],["Batch normalisation","Batch renormalisation","Batch Kalman normalisation"],["Layer normalisation","Instance normalisation","Group normalisation"],["Weight normalisation","Weight Standardisation"]];
+let modifynames2 = [[],[],["DL","DCL"],["BN","BRN","BKN"],["LN","IN","GN"],["WN","WS"]];
+let modifycolor = [[],[],["lightgreen","chartreuse"],["lightblue","deepskyblue","cornflowerblue"],["blue","steelblue","darkcyan"],["mediumslateblue","slateblue"]];
 let layerorder = [];
 
 
