9b9fbaa242ad3e54c0a64d6edca8a4a8a94f4d7c
Test <101502649+susamobus@users.noreply.github.com>
2024-03-24 09:32:13 +0800

    Update input.js

--- input.js
+++ input.js
@@ -14,15 +14,26 @@ function Tab(id) {
 }
 
 function AddLayer() {
+  layers++
   let inputfield = document.createElement("div")
   let display = document.createElement("span")
   let increment = document.createElement("button")
   let decrement = document.createElement("button")
   let input = document.createElement("input")
+  inputfield.id = "layerinput " + layers
+  display.innerHTML = "0"
+  increment.className = "smallbutton"
+  decrement.className = "smallbutton"
+  increment.onClick = function() {ChangeLayer(1,true)}
+  decrement.onClick = function() {ChangeLayer(-1,true)}
+  input.className = "tableinput"
   input.id = "structureinput"
   inputfield.appendChild(display)
   inputfield.appendChild(increment)
   inputfield.appendChild(decrement)
   inputfield.appendChild(input)
   document.getElementById("layerinput").appendChild(inputfield)
+}
+
+function ChangeLayer(value,step) {
 }
