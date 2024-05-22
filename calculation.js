a6f26305312518830460ad05541fa53b7366644e
Test <101502649+susamobus@users.noreply.github.com>
2024-05-22 14:11:47 +0800

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -216,17 +216,17 @@ function ToggleTraining() {
     trainbutton.style.borderColor = "White";
     trainbutton.style.color = "White";
     document.getElementById("trainingstatus").innerHTML = "";
-    training.stop();
- //   clearInterval(updategraph);
- //   updategraph = undefined;
+   // training.stop();
+    clearInterval(updategraph);
+    updategraph = undefined;
     istraining = false;
   } else {
     trainbutton.innerHTML = "Stop Train";
     trainbutton.style.borderColor = "Red";
     trainbutton.style.color = "Red";
     document.getElementById("trainingstatus").innerHTML = "Training...";
- //   updategraph = setInterval(UpdateGraph, 100);
-    training.start(Backprop,1);
+    updategraph = setInterval(UpdateGraph, 100);
+  //  training.start(Backprop,1);
     istraining = true;
   }
 }
