6eacc5f4a968dbc9df52a47b37c93d8d363ca894
Test <101502649+susamobus@users.noreply.github.com>
2024-05-22 15:49:18 +0800

    Update input.js

--- input.js
+++ input.js
@@ -221,7 +221,7 @@ function ToggleHelp(id) {
   let helpdiv = document.getElementById(id);
   if (helpdiv.style.display === "none") {
     currenthelpdiv = id;
-    helpdiv.style.display = "inline";
+    helpdiv.style.display = "flex";
     setTimeout(() => document.addEventListener("click", HideHelp), 1);
   } else {
     currenthelpdiv = undefined;
