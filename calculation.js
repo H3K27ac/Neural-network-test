d990e0de052a043a3d799d1bad7d6fd6c90d5c61
H3K27ac <101502649+H3K27ac@users.noreply.github.com>
2024-12-15 13:51:50 +0800

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -183,14 +183,14 @@ function SetTarget() {
 
 function NeuronCost(i,j) {
   if (i == layers-1) {
-    return 2 * (neurons[structure2[i]+j] - targets[j]);
+    return (neurons[structure2[i]+j] - targets[j]);
   } else {
     let sum = 0;
     let k2 = structure[i+1];
     for (let k=0; k<k2; k++) {
       sum += weights[structure3[i]+structure[i]*k+j+1] * activationcache[structure2b[i]+k] * costcache[structure2b[i]+k]; // NeuronCost(i+1,k)
     }
-    return sum
+    return sum;
   }
 }
 
